adab74a4ffb7db9acf918a4c83e7beb24327b43e
KristinaJelyazkova <thesecretgirlbls@abv.bg>
2018-09-23 00:54:37 +0300

    Add files via upload

--- objects.js
+++ objects.js
@@ -103,3 +103,111 @@ Heart.prototype.draw = function (context){
 	
 	context.restore();
 }
+
+function PacMan(x, y, radius, speed){	// a constructor creating pacman
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	
+	this.speed = speed;
+	this.angle = 0; 
+	this.x_speed = speed;
+	this.y_speed = 0;
+	
+	this.time = 0;
+	this.mouth = 0;
+}
+
+PacMan.prototype.turn = function(direction){
+	if(this.y_speed){ // we are travelling vertically
+		this.x_speed = -direction * this.y_speed;
+		this.y_speed = 0;
+		
+		this.angle = (this.x_speed > 0) ? 0 : Math.PI;
+	}
+	else{
+		this.y_speed = direction * this.x_speed;
+		this.x_speed = 0;
+		
+		this.angle = (this.y_speed > 0) ? 0.5 * Math.PI : 1.5 * Math.PI;
+	}
+}
+
+PacMan.prototype.turn_left = function(){
+	this.turn(-1);
+}
+
+PacMan.prototype.turn_right = function(){
+	this.turn(1);
+}
+
+PacMan.prototype.update = function (elapsed){
+	if(Math.random() < 0.01){
+		if(Math.random() < 0.5){
+			this.turn_left();
+		}
+		else{
+			this.turn_right();
+		}
+	}
+	
+	this.x += this.x_speed * elapsed;
+	this.y += this.y_speed * elapsed;
+	
+	//Checking if we are out of the canvas - pacman appears on the opposite side
+	if(this.x - this.radius > context.canvas.width){
+		this.x = -this.radius;
+	}
+	if(this.x + this.radius < 0){
+		this.x = context.canvas.width + this.radius;
+	}
+	if(this.y - this.radius > context.canvas.height){
+		this.y = -this.radius;
+	}
+	if(this.y + this.radius < 0){
+		this.y = context.canvas.height + this.radius;
+	}
+	
+	this.time += elapsed;
+	this.mouth = Math.abs(Math.sin(2 * Math.PI * this.time));
+}
+
+PacMan.prototype.draw = function (context){
+	context.save();
+	
+	context.translate(this.x, this.y);
+	context.rotate(this.angle);
+	draw_pacman(context, this.radius, this.mouth);
+	
+	context.restore();
+}
+
+function Ghost(x, y, radius, speed, color){	// a constructor creating pacman
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	
+	this.speed = speed;
+	this.color = color; 
+}
+
+Ghost.prototype.update = function (elapsed, target){
+	var angle = Math.atan2(target.y - this.y, target.x - this.x);
+	this.x_speed = Math.cos(angle) * this.speed;
+	this.y_speed = Math.sin(angle) * this.speed;
+	
+	this.x += this.x_speed * elapsed;
+	this.y += this.y_speed * elapsed;
+	
+	//Not Checking if we are out of the canvas - the ghost follows pacman
+}
+
+Ghost.prototype.draw = function (context){
+	context.save();
+	
+	context.translate(this.x, this.y);
+	context.rotate(this.angle);
+	draw_ghost(context, 0, 0, this.radius / 3, this.color);
+	
+	context.restore();
+}
